24aa58f6bf83b6f2493172f9f39fce7d0de45cf7
jahangir khedri <khedri.jahangir@gmail.com>
2023-12-17 19:27:28 +0330

    add post migration

--- module/blog/Databases/migrations/2023_12_17_130154_create_posts_table.php
+++ module/blog/Databases/migrations/2023_12_17_130154_create_posts_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title',255);
+            $table->text('content');
+            $table->integer(1)->default(0)->comment("0 => draft , 2 => published");
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
